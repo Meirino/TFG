cc2070244cc1f3b34eee7e955d8591f35f1e8e69
José Javier Meiriño Migens <jjmeirino@gmail.com>
2019-04-08 15:48:47 +0200

    Added mysql registration

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 "use strict";
 
 // Librerias
+const squel = require("squel");
 const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const cors = require("cors");
-
+const connection_data = require('./mysql').connection_data;
+const uuidv4 = require('uuid/v4');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const request = require("request");
+const mysql = require("mysql");
 const app = express();
 
 // Puerto a usar
 const port = 4000;
-const users = [
-  {
-    username: "Javier",
-    password: "password",
-    email: "jjmeiras@gmail.com",
-    avatarURL: "/avatars/00.jpg"
-  }
-];
 
 function buscarUsuario(email, callback) {
   for (var i = 0, len = users.length; i < len; i++) {
@@ -34,22 +31,20 @@ function buscarUsuario(email, callback) {
 
 // Configurar la estrategia local
 passport.use(
-  new LocalStrategy(
-    {
+  new LocalStrategy({
       // this maps the file names in the html file to the passport stuff
       usernameField: "email",
       passwordField: "password"
     },
-    function(email, password, done) {
+    function (email, password, done) {
       // replace this with our search function, mysql/monogo/service/etc
-      buscarUsuario(email, function(err, user) {
+      buscarUsuario(email, function (err, user) {
         if (err) {
           return done(err);
         }
         if (!user) {
-          console.log("bad username");
           return done(null, false, {
-            message: "Incorrect username."
+            message: "Este usuario no existe."
           });
         } else {
           if (user.password === password) {
@@ -71,6 +66,7 @@ app.use("/", express.static("public"));
 app.use("/chat", express.static("public"));
 app.use("/login", express.static("public"));
 app.use("/index", express.static("public"));
+
 // Middleware de análisis del cuerpo de Node.js
 app.use(
   cors(),
@@ -81,38 +77,38 @@ app.use(
 );
 
 // Rutas y métodos (POST, GET, PUT, DELETE, etc...)
-app.post("/api/login", function(req, res, next) {
-  console.log(req.body);
-  passport.authenticate("local", function(err, user, info) {
-    console.log(err, user, info);
+app.post("/api/login", function (req, res, next) {
+  passport.authenticate("local", function (err, user, info) {
     if (user) {
       res.send({
         user: user
       });
     } else {
       res.send({
-        error: err,
         info: info
       });
     }
   })(req, res, next);
 });
 
-app.post("/api/register", function(req, res) {
-  if (req.body.username && req.body.password) {
-    users.push({
-      username: req.body.username,
-      password: req.body.password,
-      email: req.body.email,
-      avatarURL: "/avatars/00.jpg"
+app.post("/api/register", function (req, res) {
+  const connection = mysql.createConnection(connection_data);
+  connection.connect();
+
+  if (req.body.email && req.body.password) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      bcrypt.hash(req.body.password, salt, function (err, hash) {
+        let new_user = squel.insert()
+          .into("users")
+          .set("username", req.body.username)
+          .set("usermail", req.body.email)
+          .set("password", hash)
+          .set("salt", salt)
+          .toString();
+
+        connection.query(new_user);
+      });
     });
-    res.status(200).send({
-      username: req.body.username,
-      password: req.body.password,
-      email: req.body.email,
-      avatarURL: "/avatars/00.jpg"
-    });
-    console.log(users);
   } else {
     res.status(500);
   }
