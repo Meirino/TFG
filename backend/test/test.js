69365b5ef20fbfd212e6fd8ef8e186cc1e17f31b
José Javier Meiriño Migens <jjmeirino@gmail.com>
2019-05-09 19:21:11 +0200

    Añadido un test

--- backend/test/test.js
+++ backend/test/test.js
@@ -4,3 +4,13 @@ const login_controller = require('../controladores/logincontroller');
 /*
     Añadir tests
 */
+
+describe('API Controller', function () {
+    describe('Hello World', function () {
+        it('Should return a "hello world" string', () => {
+            assert.equal(() => {
+                return "Hello World"
+            }, 'hello world');
+        });
+    });
+});
